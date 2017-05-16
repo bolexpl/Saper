794a44ce981ff74c17bd5e3514b3206748ab8001
bolek <arek2159@gmail.com>
2017-05-16 12:18:59 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.awt.*;
-import java.util.Vector;
 
 public class Main {
 
@@ -13,11 +12,6 @@ public class Main {
                 new Window();
             }
         });
-
-//        Vector<Record> v = Record.read();
-//        for(Record r : v){
-//            System.out.println(r.getDate()+", "+r.getTime()+", "+r.getBoard());
-//        }
 
 //        Record.write(new Record("tera",34,"duża"));
 //        Record.write(new Record("tera",1,"duża"));
